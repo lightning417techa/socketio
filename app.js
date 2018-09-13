4896195ef661f6d4a112f7a8e4233a9f63edaa0d
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-13 15:27:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var http = require('http');
 const app = express();
 app.get('/', function (req, res) {
   res.send('hi');
+  alert('Sending Message');
 });
 const server = http.createServer(app).listen
 (8080, function (err) {
