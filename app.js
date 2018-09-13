47b4c08f7e77353c19c0ef5907c179d99a9405b3
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-13 15:12:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
-var app = require('http').createServer(handler)
-var io = require('socket.io')(app);
-var fs = require('fs');
-
-app.listen(80);
-
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+const express = require('express');
+var http = require('http');
+const app = express();
+app.get('/', function (req, res) {
+  res.send('hi');
+  res.send('huvbhjub');
 });
+const server = http.createServer(app).listen
+(8080, function (err) {
+  if (err) {
+    console.log(err);
+https://Quarter
+hi
+     
+}
+} });
